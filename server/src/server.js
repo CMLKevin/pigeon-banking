ccd23712d89a8181f67f0198950292a9e99f5c29
Kevin Lin <kevinlin@Kevin-Lin.local>
2025-10-14 19:40:04 +0800

    Fix missing cookieParser import causing crash loop

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
